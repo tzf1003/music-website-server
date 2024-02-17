b06e25ba732be91d270536893cf39439b8d3c979
落花人 <1244201693@qq.com>
2024-02-17 12:32:51 +0800

    feat: ✨ 全局统一返回封装类 feat: ✨ 自定义异常处理 feat: ✨ 引入fastjson

--- src/main/java/com/xiaosong/music/server/controller/IndexController.java
+++ src/main/java/com/xiaosong/music/server/controller/IndexController.java
@@ -1,9 +1,12 @@
 package com.xiaosong.music.server.controller;
- 
+
+import com.xiaosong.music.server.domain.dto.ResultResponse;
+import com.xiaosong.music.server.error.BizException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
  
 @RestController
@@ -12,8 +15,12 @@ public class IndexController {
  
     @GetMapping("/hello")
     @Operation(summary = "测试接口")
-    public String index(@Parameter(name = "name", description = "名称") String name) {
-        return "hello " + name;
+    public ResultResponse index( @Parameter(name = "name", description = "名称") @RequestParam(name = "name",required = false) String name) {
+        //如果name为空就手动抛出一个自定义的异常！
+        if(name==null){
+            throw  new BizException("-1","name不能为空！");
+        }
+        return ResultResponse.success("hello " + name);
     }
  
 }
